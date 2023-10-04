5edad8275ec729ec46365293a84bee4f2f2120ae
Pierre-Oivier Dupont <pierre-olivier.dupont@krag.io>
2023-10-04 09:01:04 -0400

    Left Trigger implementation

--- racecar_teleop/scripts/slash_teleop.py
+++ racecar_teleop/scripts/slash_teleop.py
@@ -93,7 +93,7 @@ class teleop(object):
             #If left trigger is active 
             elif (joy_msg.buttons[6]):
                 # Closed-loop velocity 1 m/s, Open-loop steering
-                self.cmd_msg.linear.x  = 5 #[m/s]
+                self.cmd_msg.linear.x  = 2 #[m/s]
                 self.cmd_msg.angular.z = 0
                 self.cmd_msg.linear.z  = 1 # Control mode
                 
